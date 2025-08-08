8a58f4ffe83c2c699a79bf02bee27e3360a31f68
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2025-08-08 15:49:12 +0800

    Bump version 1.5.0 -> 1.6.0

--- mscore/__init__.py
+++ mscore/__init__.py
@@ -29,7 +29,7 @@ from sf2utils.sf2parse import Sf2File
 from console_quiet import ConsoleQuiet
 from node_soso import SmartNode, dump
 
-__version__ = "1.5.0"
+__version__ = "1.6.0"
 
 CHANNEL_NAMES = [	'normal', 'open', 'mute', 'arco', 'tremolo', 'crescendo',
 					'marcato', 'staccato', 'flageoletti', 'slap', 'pop', 'pizzicato']
