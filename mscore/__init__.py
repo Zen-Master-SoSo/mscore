990148e7f84f3ddc055e21e6a1f27d3a4a8a06b8
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2025-12-05 13:22:37 +0800

    Bump version 1.17.0 -> 1.17.1

--- mscore/__init__.py
+++ mscore/__init__.py
@@ -36,7 +36,7 @@ from sf2utils.sf2parse import Sf2File
 from console_quiet import ConsoleQuiet
 from node_soso import SmartNode, SmartTree
 
-__version__ = "1.17.0"
+__version__ = "1.17.1"
 
 CHANNEL_NAMES = ['normal', 'open', 'mute', 'arco', 'tremolo', 'crescendo',
 				 'marcato', 'staccato', 'flageoletti', 'slap', 'pop', 'pizzicato']
