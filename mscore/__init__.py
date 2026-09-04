7465aaa5e9fcd6f1b8f1441cd17c384aedfe55f9
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2026-09-04 13:01:26 +0800

    Bump version 1.20.0 -> 1.20.1

--- mscore/__init__.py
+++ mscore/__init__.py
@@ -37,7 +37,7 @@ from appdirs import user_config_dir, user_data_dir
 from console_quiet import ConsoleQuiet
 from node_soso import SmartNode, SmartTree
 
-__version__ = "1.20.0"
+__version__ = "1.20.1"
 
 
 CHANNEL_NAMES = ['normal', 'open', 'mute', 'arco', 'tremolo', 'crescendo',
